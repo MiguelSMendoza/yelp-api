15c74e6c803704b639e2c265ca882c5785b5e84d
Miguel S. Mendoza <miguel@smendoza.net>
2017-08-11 14:57:59 +0200

    Working Search

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 var express = require('express'),
     mongo = require('mongodb').MongoClient,
     JSONStream = require('JSONStream'),
-    fs = require('fs');
+    fs = require('fs'),
+    request = require('request');
 
+var ACCESS_TOKEN = '';
 
 var app = express();
 
@@ -13,14 +15,55 @@ app.get('/yelp/', function(req, res) {
     fileStream.pipe(res);
 });
 
+function authApp() {
+    if (!ACCESS_TOKEN) {
+        request.post(
+            'https://api.yelp.com/oauth2/token', {
+                form: {
+                    client_id: process.env.CLIENT_ID,
+                    client_secret: process.env.CLIENT_SECRET
+                }
+            },
+            function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var data = JSON.parse(body);
+                    ACCESS_TOKEN = data.access_token;
+                    fs.writeFile(".token", data, function(err) {
+                        if (err) {
+                            return console.log(err);
+                        }
+                    });
+                }
+            }
+        );
+    }
+}
+
 app.get('/yelp/search/*', function(req, res) {
     var value = req.params[0];
-    var offset = req.query.offset;
-    var rows = 10;
-    var page = 0;
-    res.send(JSON.stringify(data));
+    request.get({
+            url: 'https://api.yelp.com/v3/businesses/search',
+            headers: {
+                Authorization: ' Bearer ' + ACCESS_TOKEN
+            },
+            qs: {
+                locale: 'es_ES',
+                location: value
+            }
+        },
+        function(error, response, body) {
+            var data = JSON.parse(body);
+            if (!error && response.statusCode == 200) {
+                res.send(data.businesses);
+            } else {
+                res.send(data.error.description);
+            }
+
+        }
+    );
 });
 
 app.listen(process.env.PORT, function() {
+    authApp();
     console.log('Yelp API Server listening on port ' + process.env.PORT + '!');
 });
