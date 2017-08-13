9bb81f3e9ce3e7fdea09f041810accc3894eeb04
Miguel S. Mendoza <miguel@smendoza.net>
2017-08-14 01:13:52 +0200

    Almost Finished

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ var ACCESS_TOKEN = '';
 
 var app = express();
 
+/*
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+*/
 
 app.get('/yelp/', function(req, res) {
     res.writeHead(200, { 'content-type': 'text/html' })
